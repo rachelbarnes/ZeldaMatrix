983d7fc43cacf28c6616aa32d737545c5e881711
unknown <Administrator@phiws0035.sst.stp>
2016-01-21 08:45:22 -0500

    The first commit of the project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class EntryPoint {
+        static void Main(string[] args) {
+            Console.Out.WriteLine("test");
+            Console.Out.WriteLine(MathThings.Add(1, 3));
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+    }
+    class MathThings {
+        public static int Add(int x, int y) {
+            return x + y;
+        }
+        public static int Div(int x, int y) {
+            return x / y;
+        }
+        public static int Abs(int x) {
+            return x;
+        }
+
+    }
+}
