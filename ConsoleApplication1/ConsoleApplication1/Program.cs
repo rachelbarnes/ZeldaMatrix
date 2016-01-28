78b058739730f7699ff8e66a7c673e1be6926c6d
Rachel Barnes <rachelbarnesjn116@yahoo.com>
2016-01-28 08:40:51 -0500

    Fixing the Player Movements

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -7,90 +7,164 @@ using System.Threading.Tasks;
 namespace ConsoleApplication1 {
     public class EntryPoint {
         static void Main(string[] args) {
-            //boardHelperVar contains the object, so the it retains the instance of the object,
-            // which is the specific existance of one, in a basic sense.
-            var boardHelperVar = new BoardHelper();       
-            var board = boardHelperVar.InstantiateBoard();
+            var board = new Board(56);
 
-            var player = new Player();
-            var input = ""; 
+            var player = new Player(board.size);
+
+ 
+            var input = "";
             while (input != "q") {
-                Console.Out.WriteLine("Type 'q' and enter to quit;");
                 if (input == "h") {
                     player.MoveLeft();
                 }
                 if (input == "j") {
                     player.MoveDown();
-                }             
+                }
                 if (input == "k") {
                     player.MoveUp();
-                }                    
+                }
                 if (input == "l") {
                     player.MoveRight();
                 }
-                  
-                boardHelperVar.DrawBoard(board, player);
+
+                board.Draw(player);
                 input = Console.ReadLine();
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
             }
         }
     }
+    public class Board {
+        private string[,] board;
+        public int size;
+
+        public Board(int size) {
+            this.size = size;
+            board = new string[size, size];
+            for (var r = 0; r < size; r++) {
+                for (var c = 0; c < size; c++) {
+                    this.board[r, c] = " ";
+                }
+            }
+        }
+
+        public void Draw(Player player) {
+            Console.Out.WriteLine(" ");
+            //Console.Out.WriteLine( " ----------");
+            var boundingBar = " ";
+            for (var x = 0; x < size; x++) {
+                boundingBar += "-";
+            }
+            Console.Out.WriteLine(boundingBar);
+
+            for (var r = 0; r < size; r++) {
+                var rowString = "";
+                for (var c = 0; c < size; c++) {
+                    if (player.IsPlayerPosition(c, r)) {
+                        rowString += "@";
+                    }
+                    else {
+                        rowString += this.board[r, c];
+                    }
+                }
+                Console.Out.WriteLine(String.Format("|{0}|", rowString));
+            }
+            Console.Out.WriteLine(boundingBar);
+            Console.Out.WriteLine("Type 'q' and enter to quit;");
+            Console.SetCursorPosition(0, Console.CursorTop - (size + 4));
+        }
+
+
+    }
     public class Player {
-        private int playerC = 5;
-        private int playerR = 5;
+        private int playerC;
+        private int playerR;
+        private int size;
+
+        public Player(int size) {
+            playerC = (size / 2);
+            playerR = (size / 2);
+            this.size = size;
+        }
+
         public bool IsPlayerPosition(int c, int r) {
             return playerR == r && playerC == c;
         }
-
         public void MoveUp() {
             if (playerR > 0) {
                 playerR--;
             }
         }
         public void MoveDown() {
-            if (playerR < 9) {
+            if (playerR < (size - 1)) {
                 playerR++;
             }
+
         }
         public void MoveLeft() {
             if (playerC > 0) {
                 playerC--;
-            }         
+            }
         }
         public void MoveRight() {
-            if (playerC < 9) {
+            if (playerC < (size - 1)) {
                 playerC++;
             }
         }
-       }
-    public class BoardHelper {
-        //this one takes a string array to make the board
-        public void DrawBoard(string[,] board, Player player) {
-            Console.Out.WriteLine(" ----------");
-
-            for (var r = 0; r < 10; r++) {
-                var rowString = "";
-                for (var c = 0; c < 10; c++) {
-                    if (player.IsPlayerPosition(c, r)) {
-                        rowString += "@";
-                    } else {
-                        rowString += board[r, c];
-                    }
-                }
-                Console.Out.WriteLine(String.Format("|{0}|", rowString));
-            }
-            Console.Out.WriteLine(" ----------");
-        }
-        // this oen makes the string array to make the board, hence the "creation"
-        // of the rows and columns 
-        public string[,] InstantiateBoard() {
-            string[,] board = new string[10, 10];
-            for (var r = 0; r < 10; r++) {
-                for (var c = 0; c < 10; c++) {
-                    board[r, c] = " ";
-                }
-            }
-            return board;
-        }
     }
 
+    public class Collectable {
+        private int collectR;
+        private int collectC;
+        int size;
+
+        private Collectable(int size) {
+            this.size = size;
+        }
+
+    }
 }
+    //    public object CollectableStartPosition(int startPositionCollectableC, int startPositionCollectableR) {
+    //        collectableCItem = startPositionCollectableC;
+    //        collectableRItem = startPositionCollectableR;
+     //   }
+        // return CollectableStartPosition(startPositionCollectableC, startPositionCollectableR);
+
+    //    public bool CollectablePosition(int collectC, int collectR) {
+    //        return collectableCItem == collectC && collectableRItem == collectR;
+    //    }
+
+    //    public void DrawCharacter(Collectable collectable) {
+    //        for (collectR = 0; collectR < size; collectR++) {
+    //            var rowStringCollect = "";
+    //        }
+    //        for (collectC = 0, collectC < size; collectC++) {
+    //          if (collectable.CollectablePosition(collectC, collectR)) {
+    //                rowStringCollect = "!"; }
+
+    //            else {
+    //                return rowStringCollect += board[collectC, collectR];
+    //            }
+    //        }
+    //        if (collectable.CollectablePosition(collectC, collectR)) {
+
+    //        } } } }
+    //    public void Draw(Player player) {
+    //          Console.Out.WriteLine(boundingBar);
+     
+    //        for (var r = 0; r < size; r++) {
+    //            var rowString = "";
+    //        for (var c = 0; c < size; c++) {
+    //            if (player.IsPlayerPosition(c, r)) {
+    //                    rowString += "@";
+    //                }
+    //                else {
+    //                   rowString += this.board[r, c];
+    //                }
+    //            }
+    //            Console.Out.WriteLine(String.Format("|{0}|", rowString));
+ 
+
+
+
+
+                                                    
